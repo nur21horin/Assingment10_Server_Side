b5b5119e9f7b3be0880cf2be28bd8838c85ddb99
Nur Mohammod <nurm98371@gmail.com>
2025-11-12 13:26:11 +0600

    add

--- index.js
+++ index.js
@@ -75,12 +75,7 @@ async function run() {
       res.send(result);
     });
 
-    //Delete food
-    app.delete("/foods/:id", async (req, res) => {
-      const id = req.params.id;
-      const result = await foodCollection.deleteOne({ _id: new ObjectId(id) });
-      res.send(result);
-    });
+    
 
     await client.db("admin").command({ ping: 1 });
     console.log(" Successfully connected to MongoDB!");
